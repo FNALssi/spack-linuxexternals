c3687057fb7cc6b1a113d6c649777a51e514426e
Marc Mengel <mengel@fnal.gov>
2025-07-21 21:31:04 -0500

    updates for 1.x

--- tests/test_commandline.py
+++ tests/test_commandline.py
@@ -21,25 +21,31 @@ def workenv():
     os.system(f"spack env remove -y {we}")
     return None
 
+@pytest.fixture
+def workdir():
+    we = f"/tmp/work_{os.getpid()}"
+    os.mkdir(we)
+    yield we
+    os.system(f"rm -rf {we}")
+    return None
+
 def test_help():
     with os.popen("spack linuxexternals --help", "r") as fin:
         data = fin.read()
     assert(data.find("--scope") >= 0)
     assert(data.find("--help") >= 0)
 
-def test_minimal( workenv, mock_rpm):
-    with os.popen(f"spack location --env {workenv}", "r") as fin:
-         epath = fin.read().strip()
-    print(f"environment path: {epath}")
-    with os.popen(f"spack --config-scope={epath} linuxexternals --scope cmd_scope_0") as fin:
+def test_minimal( workdir, mock_rpm):
+    print(f"workdir: {workdir}")
+    with os.popen(f"spack --config-scope={workdir} linuxexternals --scope cmd_scope_0") as fin:
          data1 = fin.read()
     print(data1)
 
-    with open(f"{epath}/packages.yaml", "r") as fin:
+    with open(f"{workdir}/packages.yaml", "r") as fin:
          data2 = fin.read()
 
-    assert(data1.find("no system-installed versions of libXvMC") >= 0)
-    assert(data1.find("May want to install libXvMC and libXvMC-devel") >= 0)
+    assert(data1.find("no system-installed versions of autogen") >= 0)
+    assert(data1.find("want to install autogen") >= 0)
     assert(data2.find("spec: xproto @2022.2 ") >= 0)
     assert(data2.find("spec: texlive @20200406 ") >= 0)
 
